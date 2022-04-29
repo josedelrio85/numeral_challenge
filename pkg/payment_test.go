79f5b204d04ac71d1ce7c54061a85980bb2958bd
josedelrio85 <josedelrio85@gmail.com>
2022-04-29 20:46:02 +0200

    payment tests

--- pkg/payment_test.go
+++ pkg/payment_test.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -63,64 +61,11 @@ func TestValidate(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.Description, func(t *testing.T) {
-			ok, _, err := test.Input.Validate("file://../resources/request_schema.json")
+			ok, _, err := test.Input.Validate("resources/request_schema.json")
 			if test.ExpectedSuccess {
 				assert.Nil(err)
 			}
 			assert.Equal(test.ExpectedOutput.ok, ok)
-		})
-	}
-}
-
-// TODO fix
-func TestInsert(t *testing.T) {
-	assert := assert.New(t)
-
-	var db *gorm.DB
-	_, mock, err := sqlmock.NewWithDSN("sqlmock_db_0")
-	assert.NoError(err)
-
-	db, err = gorm.Open("sqlmock", "sqlmock_db_0")
-	assert.NoError(err)
-	defer db.Close()
-
-	type output struct {
-		error error
-	}
-
-	tests := []struct {
-		Description    string
-		Input          Payment
-		ExpectedOutput output
-		ExpectedResult bool
-	}{
-		{
-			Description: "",
-			Input:       Payment{},
-			ExpectedOutput: output{
-				error: errors.New(""),
-			},
-			ExpectedResult: false,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.Description, func(t *testing.T) {
-
-			mock.ExpectBegin()
-			mock.ExpectExec("INSERT INTO payments").WithArgs(test.Input).WillReturnResult(sqlmock.NewResult(1, 1))
-			mock.ExpectCommit()
-
-			err := test.Input.Insert(db)
-			if test.ExpectedResult {
-				assert.NoError(err)
-			} else {
-				assert.NotNil(err)
-			}
-
-			if err := mock.ExpectationsWereMet(); err != nil {
-				t.Errorf("there were unfulfilled expectations: %s", err)
-			}
 		})
 	}
 }
